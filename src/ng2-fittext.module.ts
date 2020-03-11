5ad79725000c0afc24df39db2071728b67a56b7d
Giacomo Ferlaino <giacomo.ferlaino@outlook.com>
2020-03-12 00:00:35 +0100

    ref: Formatted code using Prettier rules

--- src/ng2-fittext.module.ts
+++ src/ng2-fittext.module.ts
@@ -1,23 +1,17 @@
 import { Ng2FittextDirective } from './directives/ng2-fittext.directive';
-import {CommonModule} from '@angular/common';
-import {NgModule} from "@angular/core";
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
 @NgModule({
-    declarations: [
-        Ng2FittextDirective
-    ],
-    exports: [
-        Ng2FittextDirective
-    ],
-    imports: [
-        CommonModule
-    ]
+  declarations: [Ng2FittextDirective],
+  exports: [Ng2FittextDirective],
+  imports: [CommonModule],
 })
 export class Ng2FittextModule {
-    static forRoot() {
-        return {
-            ngModule: Ng2FittextModule,
-            providers: []
-        };
-    }
+  static forRoot() {
+    return {
+      ngModule: Ng2FittextModule,
+      providers: [],
+    };
+  }
 }
