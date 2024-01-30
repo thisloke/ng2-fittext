7c20b83d33f88e77aa9d39dea4d7cc04bf03cb70
Lorenzo Iovino <thisloke@gmail.com>
2024-01-30 15:22:06 +0100

    Fix .gitignore and build demo project  (#98)
    * Update package-lock.json
    
    * Add gitignore editor folders
    
    * Fix demo build missing dependency

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { Ng2FittextModule } from 'lib';
+import { Ng2FittextModule } from '../../projects/lib/src/ng2-fittext.module';
 
 @NgModule({
   declarations: [
